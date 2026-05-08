db778dd6a57c8ac3e447ce15a3cca41ed92930ee
unknown <tothinktodo@163.com>
2026-05-08 23:47:09 +0800

    feat: 表单隐藏输入框渐进增强， 使用useformstate 和useformStatus

--- components/NoteEditor.js
+++ components/NoteEditor.js
@@ -2,24 +2,40 @@
 
 import { useState } from "react";
 import NotePreview from "@/components/NotePreview";
-import { useFormStatus } from "react-dom";
-import { deleteNote, saveNote } from '../app/actions'
+import { useFormState } from "react-dom";
+import { deleteNote, saveNote } from "../app/actions";
+import SaveButton from "@/components/SaveButton";
+import DeleteButton from "@/components/DeleteButton";
+
+const initialState = {
+  message: null,
+};
 
 export default function NoteEditor({ noteId, initialTitle, initialBody }) {
-  const { pending } = useFormStatus();
+  const [saveState, saveFormAction] = useFormState(saveNote, initialState);
+  const [delState, delFormAction] = useFormState(deleteNote, initialState);
+
   const [title, setTitle] = useState(initialTitle);
   const [body, setBody] = useState(initialBody);
+
   const isDraft = !noteId;
 
   return (
     <div className="note-editor">
       <form className="note-editor-form" autoComplete="off">
+        <div className="note-editor-menu" role="menubar">
+          <input type="hidden" name="noteId" value={noteId} />
+          <SaveButton formAction={saveFormAction} />
+          <DeleteButton isDraft={isDraft} formAction={delFormAction} />
+        </div>
+        <div className="note-editor-menu">{saveState?.message}</div>
         <label className="offscreen" htmlFor="note-title-input">
           Enter a title for your note
         </label>
         <input
           id="note-title-input"
           type="text"
+          name="title"
           value={title}
           onChange={(e) => {
             setTitle(e.target.value);
@@ -29,47 +45,13 @@ export default function NoteEditor({ noteId, initialTitle, initialBody }) {
           Enter the body for your note
         </label>
         <textarea
+          name="body"
           value={body}
           id="note-body-input"
           onChange={(e) => setBody(e.target.value)}
         />
       </form>
       <div className="note-editor-preview">
-        <form className="note-editor-menu" role="menubar">
-          <button
-            className="note-editor-done"
-            disabled={pending}
-            type="submit"
-            role="menuitem"
-            formAction={() => saveNote(noteId, title, body)}
-          >
-            <img
-              src="/checkmark.svg"
-              width="14px"
-              height="10px"
-              alt=""
-              role="presentation"
-            />
-            Done
-          </button>
-          {!isDraft && (
-            <button
-              className="note-editor-delete"
-              disabled={pending}
-              role="menuitem"
-              formAction={() => deleteNote(noteId)}
-            >
-              <img
-                src="/cross.svg"
-                width="10px"
-                height="10px"
-                alt=""
-                role="presentation"
-              />
-              Delete
-            </button>
-          )}
-        </form>
         <div className="label label--preview" role="status">
           Preview
         </div>
