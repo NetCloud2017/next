a314f8e10fe718ccfabf79871c99be7c0e8817a8
unknown <tothinktodo@163.com>
2026-07-04 23:57:34 +0800

    feat: 更改middleware 文件为 proxy, prisma 文件新增

--- proxy.js
+++ proxy.js
@@ -46,8 +46,9 @@
 //   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 // };
 
-export { auth as middleware } from "auth";
+export { auth as proxy } from "auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
